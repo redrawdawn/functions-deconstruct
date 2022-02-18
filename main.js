70cae0bd9985e4a5bf5c8aa2b967b352b5fc2d0f
Andrew Ward <redrawdawn@gmail.com>
2022-02-18 07:48:33 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const takeStep1 = () => {
+    console.log('step')
+}
+
+const takeStep2 = () => {
+    console.log('step')
+}
+
+const takeStep3 = () => {
+    console.log('step')
+}
+
+const takeStep4 = () => {
+    console.log('step')
+}
+
+takeStep1()
+takeStep2()
+takeStep3()
+takeStep4()
